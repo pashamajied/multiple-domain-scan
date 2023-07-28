1513b320ec879958b0f949131c10ac1d9ea5f53e
Pasha Majied <42023756+pashamajied@users.noreply.github.com>
2023-07-28 17:07:07 +0700

    Update install-first.sh

--- install-first.sh
+++ install-first.sh
@@ -1,116 +1,84 @@
 #!/bin/bash
 
-# Function to check if a package is installed
-is_installed() {
-    dpkg -l | grep -q "^ii\s*$1"
+VERSION_SUBFINDER="2.6.0"
+VERSION_HTTPX="1.3.3"
+VERSION_NUCLEI="2.9.9"
+
+# Start Function
+# Function to check if a package is installed and install it if needed
+function check_and_install() {
+  command -v "$1" &>/dev/null || (sudo apt install -y "$1" || sudo yum install -y "$1" || sudo zypper install -y "$1") && echo "$1 installed successfully."
 }
 
-# Function to install required packages if missing
-install_packages() {
-    local packages=("unzip" "wget" "curl")
-
-    for package in "${packages[@]}"; do
-        if ! is_installed "$package"; then
-            sudo apt update
-            sudo apt install -y "$package"
-        fi
-    done
-}
-
-# Function to download multiple files using wget
-download_files() {
-    wget https://github.com/projectdiscovery/subfinder/releases/download/v2.6.0/subfinder_2.6.0_linux_amd64.zip
-    wget https://github.com/projectdiscovery/httpx/releases/download/v1.3.3/httpx_1.3.3_linux_amd64.zip
-    wget https://github.com/projectdiscovery/nuclei/releases/download/v2.9.9/nuclei_2.9.9_linux_amd64.zip
-}
-
-# Function to unzip and copy files to /usr/local/bin/
-install_files() {
-    unzip subfinder_2.6.0_linux_amd64.zip
-    sudo cp subfinder /usr/local/bin/
-
-    unzip httpx_1.3.3_linux_amd64.zip
-    sudo cp httpx /usr/local/bin/
-
-    unzip nuclei_2.9.9_linux_amd64.zip
-    sudo cp nuclei /usr/local/bin/
-}
-
-install_autoscan(){
-tee /usr/local/bin/runm > /dev/null << 'EOF'
-#!/bin/bash
-
-# Set warna untuk output pesan
-GREEN='\033[0;32m'
-RED='\033[0;31m'
-NC='\033[0m'
-
-echo "### Scanning Subdomain and Directory with docker ### ";
-
-#----------------------------------------
-# telegram conf
-#----------------------------------------
-CHAT_ID=397331798
-BOT_TOKEN=YOUR_BOT_TOKEN
-
-# path to save result subfinder
-LOCAL_PATH=/root/pentest
-
-read -p "Masukkan daftar domain yang akan di-scan (pisahkan dengan spasi): " domains
-
-# Convert domains into an array
-read -ra DOMAIN_ARRAY <<< "$domains"
-
-for domain in "${DOMAIN_ARRAY[@]}"; do
-  if [ ! -d "$LOCAL_PATH/$domain/" ];
-  then
-    mkdir -p "$LOCAL_PATH/$domain/"
+# Function to download and install files based on architecture (AMD or ARM)
+function download_and_install() {
+  ARCH=$(uname -m)
+  if [ "$ARCH" == "x86_64" ]; then
+    ARCH_SUFFIX="_linux_amd64"
+  else
+    ARCH_SUFFIX="_linux_arm64"
   fi
 
-  # Menampilkan pesan bahwa proses scanning dimulai
-  echo -e "${GREEN}Running...${NC}"
-  echo ""
+  wget "https://github.com/projectdiscovery/subfinder/releases/download/v$VERSION_SUBFINDER/subfinder_$VERSION_SUBFINDER$ARCH_SUFFIX.zip"
+  wget "https://github.com/projectdiscovery/httpx/releases/download/v$VERSION_HTTPX/httpx_$VERSION_HTTPX$ARCH_SUFFIX.zip"
+  wget "https://github.com/projectdiscovery/nuclei/releases/download/v$VERSION_NUCLEI/nuclei_$VERSION_NUCLEI$ARCH_SUFFIX.zip"
 
-  # Menjalankan subdomain enumeration menggunakan subfinder
-  echo -ne "${GREEN}Running subdomain enumeration for domain: $domain...${NC}"
-  subfinder -d "$domain" -o "$LOCAL_PATH/$domain/output_$domain.txt" -silent > /dev/null
-  echo -e "${GREEN} Done.${NC}"
-  echo ""
+  unzip "subfinder_$VERSION_SUBFINDER$ARCH_SUFFIX.zip"
+  unzip "httpx_$VERSION_HTTPX$ARCH_SUFFIX.zip"
+  unzip "nuclei_$VERSION_NUCLEI$ARCH_SUFFIX.zip"
 
-  # Menjalankan penyaringan subdomain aktif menggunakan httpx
-  echo -ne "${GREEN}Filtering active subdomains for domain: $domain...${NC}"
-  httpx -l "$LOCAL_PATH/$domain/output_$domain.txt" -o "$LOCAL_PATH/$domain/active_$domain.txt" -silent -mc 200 > /dev/null
-  echo -e "${GREEN} Done.${NC}"
-  echo ""
-
-  # Menjalankan vulnerability scanning menggunakan nuclei
-  echo -ne "${GREEN}Scanning for vulnerabilities for domain: $domain...${NC}"
-  nuclei -l "$LOCAL_PATH/$domain/active_$domain.txt" -es info -o "$LOCAL_PATH/$domain/nuclei_$domain.txt" &>/dev/null
-  echo -e "${GREEN} Done.${NC}"
-  echo ""
-
-  echo -e "${GREEN}Scanning $domain selesai!${NC}"
-  echo ""
-
-  # Kirim output ke telegram
-  TEXT="Result dirsearch your scan:%0A$(cat "$LOCAL_PATH/$domain/nuclei_$domain.txt")"
-  curl -s -X POST -F document=@"$LOCAL_PATH/$domain/nuclei_$domain.txt" -F "chat_id=$CHAT_ID" "https://api.telegram.org/bot$BOT_TOKEN/sendDocument"
-  echo ""
-done
-
-EOF
-chmod +x /usr/local/bin/runm
-    
+  sudo cp subfinder httpx nuclei /usr/local/bin/
+  echo "All files downloaded and installed successfully."
 }
 
-main() {
-    install_packages
-    download_files
-    install_files
-    install_autoscan
-    
-    echo "Installation completed successfully!"
+# Function to install runm based on user choice
+function install_runm() {
+  DESTINATION="/usr/local/bin/runm"
+
+  if [ "$1" == "1" ]; then
+    URL="https://tools.pashamajied.com/runm-bot-telegram.txt"
+  elif [ "$1" == "2" ]; then
+    URL="https://tools.pashamajied.com/runm-no-bot-telegram.txt"
+  else
+    echo "Invalid choice. Exiting..."
+    exit 1
+  fi
+
+  wget "$URL" -O "$DESTINATION"
+  chmod +x "$DESTINATION"
+
+  echo "The file runm.txt has been downloaded and saved to $DESTINATION with execution permission."
 }
 
-main
+# Function to replace placeholders with user input
+function replace_values() {
+  sed -i "s/YOUR_CHAT_ID/$1/g" /usr/local/bin/runm
+  sed -i "s/YOUR_BOT_TOKEN/$2/g" /usr/local/bin/runm
+}
+# End Function
 
+check_and_install "unzip"
+check_and_install "wget"
+check_and_install "curl"
+
+echo "All required packages are installed."
+
+echo "Choose one option:"
+echo "1. Send to Telegram bot"
+echo "2. Do not send to Telegram bot"
+
+read -p "Enter your choice (1 or 2): " choice
+
+if [ "$choice" == "1" ]; then
+  # Get user input for chat ID and bot token
+  read -p "Enter YOUR_CHAT_ID: " chat_id
+  read -p "Enter YOUR_BOT_TOKEN: " bot_token
+
+  # Call the function to replace the placeholders in the runm file
+  replace_values "$chat_id" "$bot_token"
+
+  echo "YOUR_CHAT_ID and YOUR_BOT_TOKEN have been successfully replaced in the runm file."
+fi
+
+download_and_install
+install_runm "$choice"
